74b9db8fdad48e2d39a9b7524df934697664dbff
Manikanta Asthavaram <manikanta.asthavaram@ibm.com>
2023-02-20 14:00:53 +0530

    accekeys updation

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
   access_key = "AKIAUBTNY75EOGX4EC57"
-  secret_key = "aRyALsV6l0yX5VhNxc/g8B/WrfKiKdSUeMd"
+  secret_key = "aRyALsV6l0yX5VhNxc/g8B/Willitwork/rfKiKdSUeMd"
 }
